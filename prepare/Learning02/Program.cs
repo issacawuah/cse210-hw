b4eee458e91c68606df3c186019f6d73396f670d
issacawuah <email>
2024-09-25 14:56:27 +0100

    Job resume done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,19 @@
-using System;
-
-class Program
+ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Creating job instances
+        Job job1 = new Job("Software Engineer", "Microsoft", 2019, 2022);
+        Job job2 = new Job("Manager", "Apple", 2022, 2023);
+        
+        // This will create a resume instantly
+        Resume myResume = new Resume("Allison Rose");
+        
+        // this will add job title to the resume
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+        
+        // display resume
+        myResume.Display();
     }
 }
